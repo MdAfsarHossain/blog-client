d55e2c0f0f137f09d842b335303779fca0ef88dd
MdAfsarHossain <afsar291722@gmail.com>
2026-06-23 08:55:45 +0600

    feat: fetch dynamic data using GenerateStaticParams() for SSG

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import BlogDetailsCard from '@/components/modules/Blogs/BlogDetailsCard';
 import { getBlogById } from '@/services/PostService';
-import React from 'react';
+
+export const generateStaticParams = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const { data: blogs } = await res.json();
+
+  return blogs.slice(0, 2).map((blog: any) => ({
+    blogId: String(blog.id),
+  }));
+};
 
 const BlogDetailsPage = async ({params}: {params: Promise<{blogId: string}>}) => {
     const {blogId} = await params;
