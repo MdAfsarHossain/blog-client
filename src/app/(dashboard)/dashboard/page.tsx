e7d270ac85bb5a870b285195ec0046ea889b026b
MdAfsarHossain <afsar291722@gmail.com>
2026-07-09 23:12:34 +0600

    feat: access session user & logout safely

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,22 @@
-const DashboardHomePage = () => {
+import { authOptions } from "@/helpers/authOptions";
+import { getUserSession } from "@/helpers/getUserSession";
+import { getServerSession } from "next-auth";
+
+const DashboardHomePage = async () => {
+  const userName = "Md. Afsar Hossain";
+  const quote = "The secret of getting ahead is getting started. – Mark Twain";
+
+  // const session = await getServerSession(authOptions)
+  // console.log(session);
+
+  const session = await getUserSession();
+
   return (
-    <div>
-      <h1>Dashboard Home Page</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6 w-full">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome, {session?.user?.name}!
+      </h1>
+      <p className="text-lg text-gray-600 italic text-center">{session?.user?.email}</p>
     </div>
   );
 };
