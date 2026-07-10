4922fd796dfa1ba40734197fc736e13f095b81e0
MdAfsarHossain <afsar291722@gmail.com>
2026-07-11 00:44:56 +0600

    feat: Implement nextAuth credentials login system

--- src/helpers/authOptions.ts
+++ src/helpers/authOptions.ts
@@ -1,12 +1,69 @@
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
+        }),
+        CredentialsProvider({
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "afsar@gmail.com" },
+                password: { label: "Password", type: "password", placeholder: "123456" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials?.password) {
+                    console.error("Invalid credentials");
+                    return null
+                }
+ 
+                try {
+                    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: credentials.email,
+                            password: credentials.password,
+                        }),
+                    })
+
+                    console.log("Response From Backend: ", res);
+                    if(!res?.ok) {
+                        console.error("Error from backend: ", res);
+                        return null;
+                    }
+                    
+                    const data = await res.json();
+                    console.log("Data From Backend: ", data);
+                    // return data;
+                    if(data.id) {
+                        return {
+                            id: data?.id,
+                            name: data?.name,
+                            email: data?.email,
+                            image: data?.image
+                        }
+                    } else {
+                        return null;
+                    }
+                    
+                } catch (error) {
+                    console.log(error)
+                    return null
+                }
+    }
+  })
     ],
+    callbacks: {
+        async jwt({token, user}: {token: any, user: any}) {
+            if(user) {
+                token.id = user.id;
+            }
+        }
+    },
     secret: process.env.AUTH_SECRET,
     pages: {
         signIn: "/login"
